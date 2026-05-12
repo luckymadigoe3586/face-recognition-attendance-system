2e69bfe8400b148693a2c660a6efa3f296b3d1f7
luckymadigoe3586 <luckymadigoe3586@gmail.com>
2026-05-12 13:02:20 +0200

    Setup FastAPI ML service

--- backend/AttendanceSystem/Attendance.API/Program.cs
+++ backend/AttendanceSystem/Attendance.API/Program.cs
@@ -34,7 +34,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    k
+        
+        app.UseSwaggerUI();'jhftyuo85dcb'
 }
 
 app.UseHttpsRedirection();
